fd083b6da6217c080a405db95758964a38ba2db0
Carole16 <ckapedo-amah@st.ug.edu.gh>
2023-02-04 02:43:07 -0800

    task 1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -8,7 +8,6 @@
  * Return: 0
  */
 
-		
 
 char *_strdup(char *str)
 
@@ -25,14 +24,12 @@ char *_strdup(char *str)
 		i++;
 
 	aaa = malloc(sizeof(char) * (i + 1));
-
-		
-	if (aaa == NULL)
+		if (aaa == NULL)
 		return (NULL);
 
 	for (r = 0; str[r]; r++)
 		aaa[r] = str[r];
 
-		
+
 	return (aaa);
 }
